7628ede0cbf2c270b85ce1025487f2f269547362
ivan-moser <ivaan.moser@gmail.com>
2024-09-05 12:29:06 +0200

    Add: Added some style, and in the app.js the constants and functions signs

--- app.js
+++ app.js
@@ -2,3 +2,22 @@ const listElement = document.getElementsByClassName("list");
 const time = document.getElementById("time");
 const date = document.getElementById("date");
 
+const refresh = document.getElementById("refresh");
+const check = document.getElementById("check");
+const deleteListElement = document.getElementById("delete");
+const add = document.getElementById("add");
+
+
+//CLOCK AND DATE FUNCTIONS
+
+//LIST FUNCTIONS
+function createList() {
+
+}
+
+//ADD A TODO FUNCTION
+
+//REFRESH FUNCTION
+
+
+
