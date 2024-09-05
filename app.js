9e54238e88e9cd79d161e6a5ce7cce3dd9353f8a
ivan-moser <ivaan.moser@gmail.com>
2024-09-05 17:07:14 +0200

    Add: added the add todo logics

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-const listElement = document.getElementsByClassName("list");
 const time = document.getElementById("time");
 const date = document.getElementById("date");
-
-
+const mainContent = document.getElementById("main-content");
+const mainContainer = document.getElementById("main-container");
 
 const refresh = document.getElementById("refresh");
-const check = document.getElementById("check");
+const empty = document.getElementById("empty");
 const deleteListElement = document.getElementById("delete");
 const add = document.getElementById("add");
 
@@ -29,10 +28,52 @@ setInterval(updateClock, 1000);
 //LIST FUNCTIONS
 function createList() {
 
+    const listEmpty = document.createElement("img");
+    listEmpty.classList.add("icons");
+    listEmpty.id = "empty";
+    listEmpty.setAttribute("src", "./assets/empty.png");
+
+    const listRemove = document.createElement("img");
+    listRemove.classList.add("icons");
+    listRemove.id = "delete";
+    listRemove.setAttribute("src", "./assets/delete.png");
+    listRemove.style = "margin-left: auto";
+
+    const inputField = document.createElement("input");
+    inputField.type = "text";
+    inputField.classList.add("todo-input");
+    inputField.placeholder = "Type your todo...";
+
+    const listElement = document.createElement("div");
+    listElement.style = "margin-top: 5px"
+    listElement.classList.add("list");
+
+    listElement.append(listEmpty, inputField, listRemove);
+    mainContent.appendChild(listElement);
+
+    inputField.focus();
+
+    inputField.addEventListener("keydown", function(event) {
+        if (event.key === "Enter") {
+            const message = inputField.value.trim();
+            if (message !== "") {
+                const textElement = document.createElement("p");
+                textElement.innerHTML = message;
+                listElement.replaceChild(textElement, inputField);
+            } else {
+                if (mainContent.lastChild) {
+                    mainContent.removeChild(mainContent.lastChild);
+                }
+            }
+        } else if (event.key === "Escape") {
+            mainContent.removeChild(mainContent.lastChild);
+        }
+    })
 }
 
 //ADD A TODO FUNCTION
 
-//REFRESH FUNCTION
+add.addEventListener("click", createList);
 
+//REFRESH FUNCTION
 
