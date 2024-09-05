b87cfd677eb72183e7590258492655f200dd8d5f
ivan-moser <ivaan.moser@gmail.com>
2024-09-05 13:13:29 +0200

    Add: added the date and time functions

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const listElement = document.getElementsByClassName("list");
 const time = document.getElementById("time");
 const date = document.getElementById("date");
 
+
+
 const refresh = document.getElementById("refresh");
 const check = document.getElementById("check");
 const deleteListElement = document.getElementById("delete");
@@ -9,6 +11,20 @@ const add = document.getElementById("add");
 
 
 //CLOCK AND DATE FUNCTIONS
+function updateClock() {
+    const today = new Date();
+    const year = today.getFullYear();
+    const month = String(today.getMonth() + 1).padStart(2, '0');
+    const day = String(today.getDate()).padStart(2, '0');
+
+    const hour = String(today.getHours()).padStart(2, '0');
+    const minute = String(today.getMinutes()).padStart(2, '0');
+    
+    time.innerHTML = `${hour}:${minute}`;
+    date.innerHTML = `${day}-${month}-${year}`;
+}
+updateClock();
+setInterval(updateClock, 1000);
 
 //LIST FUNCTIONS
 function createList() {
@@ -18,6 +34,5 @@ function createList() {
 //ADD A TODO FUNCTION
 
 //REFRESH FUNCTION
-
 
 
